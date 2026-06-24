8ce443274482a63787d82d81807ded86feade9a8
simondev95 <simon-dev95@proton.me>
2026-06-24 12:20:24 +0200

    Add button to create equipment in Dofus DB script
    Updated the user script to include a button for creating equipment and modified the example equipment details.

--- dofus-stuff.js
+++ dofus-stuff.js
@@ -1,13 +1,3 @@
-// ==UserScript==
-// @name         createStuff
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Créer vos équipements sur Dofus DB
-// @author       Simon-dev95
-// @match        https://beta.dofusdb.fr/*
-// @grant        none
-// ==/UserScript==
-// ATTENTION le nom des stuff son traduit de la version anglaise du jeux, pas la version française.
 (function() {
     'use strict';
 
@@ -28,57 +18,62 @@
         return params.join('&');
     }
 
-    // Créer votre équipement en utilisant la fonction de création d'équipement du site
-    var stuff = {
-        "name": "exemple",
-        "level": 200,
-        "items": [
-            {
-                "id": 16358, // Epée de Feu de L'Ombre +30
-                "amount": 1
-            },
-            {
-                "id": 16410, // Bouclier de Feu de L'Ombre +25
-                "amount": 1
-            },
-            {
-                "id": 17893, // Plaque de Maître Forgeron +30
-                "amount": 1
-            },
-            {
-                "id": 16411, // Lance de Feu de L'Ombre +25
-                "amount": 1
-            },
-            {
-                "id": 17900, // Cuisses de Feu de L'Ombre +25
-                "amount": 1
-            },
-            {
-                "id": 16408, // Gants de Fer +30
-                "amount": 1
-            },
-            {
-                "id": 17901, // Casque d'Élémentaliste +30
-                "amount": 1
-            },
-            {
-                "id": 16409, // Bâton de Feu de l'Élémentaliste +25
-                "amount": 1
-            },
-            {
-                "id": 16413, // Main de Foudre +25
-                "amount": 1
-            },
-            {
-                "id": 17899, // Anneau de L'Élémentaliste +30
-                "amount": 1
-            },
-            {
-                "id": 16414, // Bottes de Feu de l'Élémentaliste +30
-                "amount": 1
-            }
-        ]
-    };
+    // Affichage pour créer votre équipement L'équipment est à titre d'exemple.
+    $("body").append('<button id="create-equipment">Créer mon équipement</button>');
+    $("#create-equipement").on("click", function() {
+        var stuff = {
+            "name": "Forgelance",
+            "level": 200,
+            "items": [
+                {
+                    "id": 16358, // Epée de Feu de L'Ombre +30
+                    "amount": 1
+                },
+                {
+                    "id": 16410, // Bouclier de Feu de L'Ombre +25
+                    "amount": 1
+                },
+                {
+                    "id": 17893, // Plaque de Maître Forgeron +30
+                    "amount": 1
+                },
+                {
+                    "id": 16411, // Lance de Feu de L'Ombre +25
+                    "amount": 1
+                },
+                {
+                    "id": 17900, // Cuisses de Feu de L'Ombre +25
+                    "amount": 1
+                },
+                {
+                    "id": 16408, // Gants de Fer +30
+                    "amount": 1
+                },
+                {
+                    "id": 17901, // Casque d'Élémentaliste +30
+                    "amount": 1
+                },
+                {
+                    "id": 16409, // Bâton de Feu de l'Élémentaliste +25
+                    "amount": 1
+                },
+                {
+                    "id": 16413, // Main de Foudre +25
+                    "amount": 1
+                },
+                {
+                    "id": 17899, // Anneau de L'Élémentaliste +30
+                    "amount": 1
+                },
+                {
+                    "id": 16414, // Bottes de Feu de l'Élémentaliste +30
+                    "amount": 1
+                }
+            ]
+        };
+        createStuff(stuff);
+    });
+})();
 
     createStuff(stuff);
 })();
